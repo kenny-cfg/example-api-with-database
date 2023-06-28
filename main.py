3ef394bef04060a04e08f86fa809889954dd6abe
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-28 19:31:03 +0100

    Some comments

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ def ping():
 
 @app.route('/user', methods=['POST'])
 def create_user():
+    # Interpret (deserialize, marshall) the JSON into a Python map
     user_data = request.get_json()
+    # Extract name from resulting map...
     name = user_data['name']
+    # ...and place of birth
     place_of_birth = user_data['placeOfBirth']
+    # Echo the results back to prove we've received it
     return jsonify({
         'receivedName': name,
         'receivedPlaceOfBirth': place_of_birth
