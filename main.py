413bd142be39eeb7a581e1b3d3b05dbcbd9a1deb
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-28 18:44:04 +0100

    Flask installed, single endpoint

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def ping():
+    return jsonify({
+        'message': 'pong'
+    })
+
+
 if __name__ == '__main__':
-    print('HELLO WORLD')
+    app.run(port=5000, debug=True)
