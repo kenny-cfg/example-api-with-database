076b74941e7da2bfb5b4ca86a8ae6e45904d5351
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-28 19:18:16 +0100

    Some comments

--- database_utils.py
+++ database_utils.py
@@ -3,6 +3,8 @@ from config import USER, PASSWORD, HOST
 
 
 def _connect_to_db(db_name):
+    # Boilerplate code to connect to a mysql database, uses the auth details
+    # in config.py
     cnx = mysql.connector.connect(
         host=HOST,
         user=USER,
@@ -16,18 +18,24 @@ def _connect_to_db(db_name):
 def create_user(name, place_of_birth):
     db_name = 'people'
     try:
+        # Connect to the database
         with _connect_to_db(db_name) as db_connection:
+            # Make a cursor (an object with which we can execute a SQL query)
             with db_connection.cursor() as cur:
                 print("Connected to DB: %s" % db_name)
 
+                # Construct SQL query
                 query = f"""
                 insert into user (name, place_of_birth)
                 values ('{name}', '{place_of_birth}')
                        """ # TODO: Really really insecure
 
+                # Run SQL query against database
                 cur.execute(query)
+                # Tell mySQL that you're done
                 db_connection.commit()
     except Exception:
+        # If anything goes wrong
         raise RuntimeError("Failed to read data from DB")
 
 
